223c3083eccaa5bbc3dd322777d8f43c96878ca2
poi-vrc <poi-vrc@users.noreply.github.com>
2023-12-14 16:40:36 +0000

    deploy: 4d8c51da141e8afd27eeef60ccc0cc520333eb2a

--- search/files_d.js
+++ search/files_d.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['vrccabinetanimcabinetmoduleconfig_2ecs_0',['VRCCabinetAnimCabinetModuleConfig.cs',['../_v_r_c_cabinet_anim_cabinet_module_config_8cs.html',1,'']]],
-  ['vrcmergeanimlayerwearablemoduleconfig_2ecs_1',['VRCMergeAnimLayerWearableModuleConfig.cs',['../_v_r_c_merge_anim_layer_wearable_module_config_8cs.html',1,'']]]
+  ['vrcmergeanimlayerwearablemoduleconfig_2ecs_1',['VRCMergeAnimLayerWearableModuleConfig.cs',['../_v_r_c_merge_anim_layer_wearable_module_config_8cs.html',1,'']]],
+  ['vrmspringboneproxy_2ecs_2',['VRMSpringBoneProxy.cs',['../_v_r_m_spring_bone_proxy_8cs.html',1,'']]]
 ];
